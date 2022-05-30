360f9b4fdc8e6fd67fdc5c0b8bce7ffd252b5e65
5vl <nederlandmax@gmail.com>
2022-05-30 17:18:27 +0200

    Added pig bomb functionality and fixed a pom.xml thing

--- src/main/java/me/fivevl/troll/listeners/InvClickListener.kt
+++ src/main/java/me/fivevl/troll/listeners/InvClickListener.kt
@@ -1,6 +1,7 @@
 package me.fivevl.troll.listeners
 
 import me.fivevl.troll.gui.TrollGui
+import org.bukkit.entity.EntityType
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
@@ -14,9 +15,8 @@ class InvClickListener : Listener {
         if (TrollGui.inTrollGui.containsKey(p)) {
             e.isCancelled = true
             val ps = TrollGui.inTrollGui[p]!!
-            if (e.rawSlot == 0) {
-                yeetPlayer(p, ps)
-            }
+            if (e.rawSlot == 0) yeetPlayer(p, ps)
+            if (e.rawSlot == 1) pigBomb(p, ps)
         }
     }
 
@@ -25,6 +25,16 @@ class InvClickListener : Listener {
         loc.yaw = Random.nextInt(360).toFloat()
         loc.pitch = Random.nextInt(-50, -20).toFloat()
         ps.velocity = loc.direction.multiply(3)
+        TrollGui.inTrollGui.remove(p)
+        p.closeInventory()
+    }
+
+    private fun pigBomb(p: Player, ps: Player) {
+        val loc = ps.location
+        loc.add(0.0, 2.0, 0.0)
+        for (i in 0..10) {
+            loc.world.spawnEntity(loc, EntityType.PIG)
+        }
         TrollGui.inTrollGui.remove(p)
         p.closeInventory()
     }
